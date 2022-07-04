b8881f9583d38fd79d379be39daf16fc1c5ad190
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-04 10:05:59 -0300

    remove filters

--- posts/templatestags/om_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='plural_comentarios')
-def plural_comentarios(num_comentarios):
-    return f'{num_comentarios} COMENTÁRIO(S)'
