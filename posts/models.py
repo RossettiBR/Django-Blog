d6fc345846b572d9e42cbe936db0bf448e51abf8
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-05 16:04:05 -0300

    create func resize img

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -30,4 +33,26 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        
+        self.tamanho_imagem(self.imagem_post.name, 800)
+
+    @staticmethod
+    def tamanho_imagem(nome_image, nova_largura):
+
+        img_path = os.path.join(settings.MEDIA_ROOT, nome_image)
+        img = Image.open(img_path)
+
+        widht, height = img.size
+        nova_altura = round((nova_largura * height) / widht)
+
+        if widht <= nova_largura:
+            img.close()
+            return
+
+        nova_imagem = img.resize((nova_largura, nova_altura), Image.ANTIALIAS)
+
+        nova_imagem.save(
+            img_path,
+            optimize=True,
+            quality=60
+        )
+        nova_imagem.close()
